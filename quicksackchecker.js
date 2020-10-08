406c4a8ef6c7de54cd2e8218b623f0b8d33a79ff
baumbatz <baumbaTz@gmail.com>
2020-10-08 22:28:44 +0200

    Update quicksackchecker.js

--- quicksackchecker.js
+++ quicksackchecker.js
@@ -1,3 +1,4 @@
+
 console.log('QuickSackChecker starting');
 
 var Twit = require('twit');
@@ -30,23 +31,23 @@ function question(eventMsg) {
   if(msgTXT.startsWith("@" + botname)) {
     if(msgTXT.includes(keyword)) {
       var trigger = execPhp('/var/www/quicksack.li/quicktrigger.php');
-    
-      filter = msgTXT.replace('@' + msgAT, '');
-      filter = filter.replace(keyword, '');
-      filter = filter.trim();
-      filterArray = filter.split(" ");
-      
-      filterArray.forEach(createFilterString);
-      filterString = filterString.substr(14);
-      
-      console.log(filterString);
-      console.log(filter);
+		
+        filter = msgTXT.replace('@' + msgAT, '');
+        filter = filter.replace(keyword, '');
+        filter = filter.trim();
+	    filterArray = filter.split(" ");
+	    filterString = "";
+	    filterArray.forEach(createFilterString);
+	    filterString = filterString.substr(15);
+	    
+	    console.log(filterString);
+        console.log(filter);
 
         var con = mysql.createConnection(db);
 
         con.connect(function(err) {
 
-          var queryString = 'SELECT * FROM qsEpisodeList WHERE '+ filterString;
+          var queryString = 'SELECT * FROM qsEpisodeList WHERE title LIKE '+ filterString;
 
           con.query(queryString, function (err, result, fields) {
             if (err) {
@@ -55,18 +56,18 @@ function question(eventMsg) {
             else {
               //check to see if the result is empty
               if(result.length == 1){
-                var tTitle = result[0].title;
-                var tUrl = result[0].url;
-                var tPublished = result[0].published;
-                var answer = '@' + msgFROM + ' I found this Filmsack Episode:\n' + tTitle + '\nPublished: ' + tPublished + '\n' + tUrl; 
-                answerIt(answer, msgID);
-              } else if(result.length > 0) {
-                var answer = '@' + msgFROM + ' I found multiple Filmsack episodes that might fit your search:\nhttps://quicksack.li?f=' + encodeURI(filter);
-                answerIt(answer, msgID);
+				var tTitle = result[0].title;
+				var tUrl = result[0].url;
+				var tPublished = result[0].published;
+				var answer = '@' + msgFROM + ' I found this Filmsack Episode:\n' + tTitle + '\nPublished: ' + tPublished + '\n' + tUrl; 
+				answerIt(answer, msgID);
+			  } else if(result.length > 0) {
+				var answer = '@' + msgFROM + ' I found multiple Filmsack episodes that might fit your search:\nhttps://quicksack.li?f=' + encodeURI(filter);
+				answerIt(answer, msgID);
               } else {
                 console.log('No Results');
-                var answer = '@' + msgFROM + ' It seems that Movie/Show has not been sacked, yet. Or at least i could not find it with the provided query.\nhttps://quicksack.li?f=' + encodeURI(filter);
-                answerIt(answer, msgID);
+				var answer = '@' + msgFROM + ' It seems that Movie/Show has not been sacked, yet. Or at least i could not find it with the provided query.\nhttps://quicksack.li?f=' + encodeURI(filter);
+				answerIt(answer, msgID);
               }
             }
           });
@@ -82,7 +83,7 @@ function answerIt(txt, id) {
       in_reply_to_status_id: id,
   }
   T.post('statuses/update', tweet, function(err, data, response) {
-        console.log(".");
+        console.log(err);
   })
   console.log(tweet);
 }
